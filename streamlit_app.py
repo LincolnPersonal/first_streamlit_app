39db1b702656e47c41cd658c04a7c2dc61d18264
LincolnPersonal <146117517+LincolnPersonal@users.noreply.github.com>
2023-09-26 11:02:42 -0600

    Update streamlit_app.py
    Try "\" to trap single quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom"'"s New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Bluebery Oatmeal')
 streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
