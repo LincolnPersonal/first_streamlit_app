1e90197271176fdb5999052f72dcad0c656ac3c1
LincolnPersonal <146117517+LincolnPersonal@users.noreply.github.com>
2023-09-29 12:16:16 -0600

    Update streamlit_app.py
    Added if/then and stopped some preloading.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvise api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_chioce:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#Another python package for calling api
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize response for table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the normalized data
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  steamlit.error()
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
