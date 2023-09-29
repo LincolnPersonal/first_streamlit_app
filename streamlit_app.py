6464dfe02952c784b41a91a9e8c5cf63b3ef3554
LincolnPersonal <146117517+LincolnPersonal@users.noreply.github.com>
2023-09-29 12:01:14 -0600

    Update streamlit_app.py
    Adding to be able to add back to database

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,5 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+#This won't work correctly, but go with it for now
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
